5e6be25f45b691e753bd8089f556bad1eb99e88f
astroboy-eth <96096406+astroboy-eth@users.noreply.github.com>
2021-12-20 05:26:40 +0000

    Deploying to gh-pages from @ xata-fi/analytics@4eec3e37c646b58b396b61d06a74a141cf16e346 🚀

---
+++ precache-manifest.00ea9c8b277493dab83db380bd7d14c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2789139da9d5b15d2499d1992f44f3be",
+    "url": "/analytics/index.html"
+  },
+  {
+    "revision": "dd8f7e5c47704751121a",
+    "url": "/analytics/static/js/2.6f0896fb.chunk.js"
+  },
+  {
+    "revision": "afb4b93d3ad3cb6e9a50ec09df937afc",
+    "url": "/analytics/static/js/2.6f0896fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37428bf91d0c2bb68815",
+    "url": "/analytics/static/js/main.751ac9e5.chunk.js"
+  },
+  {
+    "revision": "914c31c88761027fd31d",
+    "url": "/analytics/static/js/runtime-main.8aa2e56d.js"
+  },
+  {
+    "revision": "5fc0c9bdb40040928ed73dbbcecd62a3",
+    "url": "/analytics/static/media/eth.5fc0c9bd.png"
+  },
+  {
+    "revision": "b5870747f2ce71ba845dea99229ad28d",
+    "url": "/analytics/static/media/xata-brand.b5870747.svg"
+  },
+  {
+    "revision": "0286e04cc22c4c754a0530961eff08ef",
+    "url": "/analytics/static/media/xata-logo.0286e04c.png"
+  }
+]);
