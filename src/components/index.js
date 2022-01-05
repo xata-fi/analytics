5aee5754a54495558a362534b2152d542d5939b9
astroboy-eth <96096406+astroboy-eth@users.noreply.github.com>
2022-01-05 15:33:14 +0800

    Fix/mobile safari bug (#16)
    * added custom whitelist
    
    removed weird display on safari
    
    clicking on xata brand should go back to homepage as well
    
    added min-height to fill black space for tokens,pairs,accounts page in smaller media widths
    
    removed margin for polling dot
    
    * modified according to comments

--- src/components/index.js
+++ src/components/index.js
@@ -118,8 +118,6 @@ export const SubNavEl = styled.li`
 `
 
 export const PageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
   padding-top: 36px;
   padding-bottom: 80px;
   min-height: 100vh;
--- src/contexts/Application.js
+++ src/contexts/Application.js
@@ -1,8 +1,7 @@
 import React, { createContext, useContext, useReducer, useMemo, useCallback, useState, useEffect } from 'react'
-import { timeframeOptions, SUPPORTED_LIST_URLS__NO_ENS } from '../constants'
+import { timeframeOptions, TOKEN_WHITELIST } from '../constants'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
-import getTokenList from '../utils/tokenLists'
 import { healthClient } from '../apollo/client'
 import { SUBGRAPH_HEALTH } from '../apollo/queries'
 dayjs.extend(utc)
@@ -269,12 +268,7 @@ export function useListedTokens() {
 
   useEffect(() => {
     async function fetchList() {
-      const allFetched = await SUPPORTED_LIST_URLS__NO_ENS.reduce(async (fetchedTokens, url) => {
-        const tokensSoFar = await fetchedTokens
-        const newTokens = await getTokenList(url)
-        return Promise.resolve([...tokensSoFar, ...newTokens.tokens])
-      }, Promise.resolve([]))
-      let formatted = allFetched?.map((t) => t.address.toLowerCase())
+      let formatted = TOKEN_WHITELIST
       updateSupportedTokens(formatted)
     }
     if (!supportedTokens) {
