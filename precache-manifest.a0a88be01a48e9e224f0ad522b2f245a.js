0e9090f8b02bd84171867a05931700f364914637
astroboy-eth <96096406+astroboy-eth@users.noreply.github.com>
2022-02-07 06:23:26 +0000

    Deploying to gh-pages from @ xata-fi/analytics@35e445a95e5b428789f0f04a3c0980e2d21d9730 🚀

---
+++ precache-manifest.a0a88be01a48e9e224f0ad522b2f245a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc747c8a6856e610334a3a455232f4c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a67ca2722760f9211da",
+    "url": "./static/js/2.8608fc41.chunk.js"
+  },
+  {
+    "revision": "c592e5a5975082d7e994531de0ce508d",
+    "url": "./static/js/2.8608fc41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a5add17364d6fb19756",
+    "url": "./static/js/main.c62ad58f.chunk.js"
+  },
+  {
+    "revision": "7cdfd1294f950155bb3e",
+    "url": "./static/js/runtime-main.fc23a649.js"
+  },
+  {
+    "revision": "b0a5d75c9ba39d6c8fb59ee1e1cdd63c",
+    "url": "./static/media/ata.b0a5d75c.png"
+  },
+  {
+    "revision": "5fc0c9bdb40040928ed73dbbcecd62a3",
+    "url": "./static/media/eth.5fc0c9bd.png"
+  },
+  {
+    "revision": "b5870747f2ce71ba845dea99229ad28d",
+    "url": "./static/media/xata-brand.b5870747.svg"
+  },
+  {
+    "revision": "0286e04cc22c4c754a0530961eff08ef",
+    "url": "./static/media/xata-logo.0286e04c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.40eeacdc5f702a52e02dc9424777a012.js"
+  "./precache-manifest.a0a88be01a48e9e224f0ad522b2f245a.js"
 );
 
 self.addEventListener('message', (event) => {
